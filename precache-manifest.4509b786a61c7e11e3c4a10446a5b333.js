79c9141e069466395b67e09ce80967688d81d237
Jacek Gałka <jacgalka@icloud.com>
2019-08-10 00:32:40 +0200

    Updates

---
+++ precache-manifest.4509b786a61c7e11e3c4a10446a5b333.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2ced59471c93f6209732979ddebd3d2",
+    "url": "/art-dent-age/index.html"
+  },
+  {
+    "revision": "6d815698deedd6457ae6",
+    "url": "/art-dent-age/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7684408bd5554961719d",
+    "url": "/art-dent-age/static/js/2.4f057907.chunk.js"
+  },
+  {
+    "revision": "6d815698deedd6457ae6",
+    "url": "/art-dent-age/static/js/main.87f0dab6.chunk.js"
+  },
+  {
+    "revision": "f3cc94ec4b59eaca3364",
+    "url": "/art-dent-age/static/js/runtime~main.a03064d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/art-dent-age/static/media/logo.5d5d9eef.svg"
+  }
+]);
