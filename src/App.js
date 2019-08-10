e213bc42c61b077a572c3c1d88bf671d5bfc4eef
Jacek Gałka <jacgalka@icloud.com>
2019-08-10 11:32:03 +0200

    Nawigacja

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
 import React from 'react';
-import { Component } from 'react'
+import {Component} from 'react'
 import 'flatpickr/dist/themes/material_green.css'
-import Flatpickr from 'react-flatpickr'
-import Moment from 'react-moment';
 import 'moment/locale/pl';
 import './App.css';
 import AgeToday from "./components/AgeToday";
 import AgePast from "./components/AgePast";
+import Logo from './components/Logo';
 
 
 class App extends Component {
-  constructor() {
-    super();
-  }
+    constructor() {
+        super();
+        this.state = {
+            currentPage: 'today'
+        };
 
-  render() {
-    return (
-        <div className="app">
-            <AgeToday />
-            <AgePast />
-        </div>
-    )
-  }
+        this.handleNavigation = this.handleNavigation.bind(this);
+    }
+
+    handleNavigation(currentPage) {
+        this.setState({currentPage: currentPage});
+    }
+
+    renderCurrentPage = function() {
+        if (this.state.currentPage === 'today') {
+            return <AgeToday />;
+        } else {
+            return <AgePast />;
+        }
+    };
+
+    render() {
+        return (
+            <div className="app">
+                <nav>
+                    <button className={this.state.currentPage === 'today' ? 'nav-btn active' : 'nav-btn'} onClick={() => this.handleNavigation('today')}>Wiek na dzisiaj</button>
+                    <button className={this.state.currentPage === 'past' ? 'nav-btn active' : 'nav-btn'} onClick={() => this.handleNavigation('past')}>Wiek na dzień badania</button>
+                </nav>
+                {this.renderCurrentPage()}
+            </div>
+        )
+    }
 }
 
 export default App;
