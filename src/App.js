3835d3945536a0cc0d0a662b14b32ab6844297c7
Jacek Gałka <jacgalka@icloud.com>
2019-08-10 10:05:35 +0200

    Podstawowy wiek

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Component } from 'react'
+import 'flatpickr/dist/themes/material_green.css'
+import Flatpickr from 'react-flatpickr'
+import Moment from 'react-moment';
+import 'moment/locale/pl';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      date: new Date("1996-08-30")
+    };
+  }
+
+  render() {
+    let { date } = this.state;
+    return (
+        <div className="app">
+          <Flatpickr
+              value={date}
+              onChange={date => { this.setState({date}) }}
+          />
+          <h1>
+            <h3>
+              Wiek: <Moment diff={date.toString()} unit="years">{new Date()}</Moment>
+            </h3>
+            <p>A dokładniej: <Moment
+                locale="fr"
+                date={date.toString()}
+                unit="years"
+                durationFromNow
+            /></p>
+          </h1>
+        </div>
+    )
+  }
 }
 
 export default App;
